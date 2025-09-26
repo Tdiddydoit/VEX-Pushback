0ec646cd2448236afd401aa42fec9bc047cfd0c5
TMANHARRIS <156351029+Tdiddydoit@users.noreply.github.com>
2025-09-26 10:34:25 -0700

    Add files via upload

--- Drive_4_Real.py
+++ Drive_4_Real.py
@@ -32,19 +32,19 @@ class Bot_Motors():
         # Group all motors and sides  and retrun all from function
         DriveTrain = [motor1,motor2,motor3,motor4, self.left_side, self.right_side]
         return DriveTrain
-    
+
+# Initialize robot motors and create a MotorGroup for all drive motors
+bot = Bot_Motors()
+# returns [m1, m2, m3, m4, left_group, right_group]
+Motor_list = bot.Drivetrain()
 
 def Autonomus():
 
-    # Initialize robot motors and create a MotorGroup for all drive motors
-    bot = Bot_Motors()
-    drive_list = bot.Drivetrain()  # returns [m1, m2, m3, m4, left_group, right_group]
-
     # The first four items are the individual motors
-    motors = drive_list[0:4]
-    All_Wheels = MotorGroup(*motors)
+    motors = Motor_list[0:4]
+    All_Wheels = MotorGroup(motors)
 
-    brain.screen.clear_line(1)
+    brain.screen.clear_line()
     brain.screen.print("Autonomous start")
 
     # Autonomus commands
@@ -71,16 +71,13 @@ def Autonomus():
 def User_Control():
     # Print user contol on screen
     brain.screen.print("User Control")
-    brain.screen.new_line()
+   
 
-    # Initialize drivetrain
-    DriveTrain = Bot_Motors()
-    drive_list = DriveTrain.Drivetrain()
 
 
     # grab left and right side from dirvetrain class
-    left_side = drive_list[4]
-    right_side = drive_list[5]
+    left_side = Motor_list[4]
+    right_side = Motor_list[5]
 
     # Constanly read and apply  joystick postions while in user control
     while True:
@@ -108,12 +105,15 @@ def User_Control():
 
         wait(20, TimeUnits.MSEC)  # Small delay for smoother updates
 
+        if LeftJoy & RightJoy == 0:
+            wait(10, SECONDS)
+            break
+
 
 
 if __name__ == "__main__":
     def main():
         # Call everything in and run main 
-        # main will never make it program stop due to while loop in user control timeout can be implemented
         Autonomus()
         User_Control()
         brain.program_stop()
